7dcacc3fb1a53cd7f359efdd2a5d0597374d0de1
ssurendran <ssurendran@armor.com>
2018-11-26 13:08:51 +0530

    First single instance EC2 main.tf

---
+++ main.tf
@@ -0,0 +1,12 @@
+provider "aws" {
+  region = "ap-south-1"
+}
+
+resource "aws_instance" "example1" {
+  ami           = "ami-06bcd1131b2f55803"
+  instance_type = "t2.micro"
+
+  tags {
+      Name ="terraform-example1"
+  }
+}
